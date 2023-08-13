2456dd4b1000a8eec2c0dde11be3046f8e31d800
Naufal Athallah Iwel <spealdm6@gmail.com>
2023-08-13 14:30:39 +0700

    Merged master into main

---
+++ Libs/TempTestSuite1691910851909.groovy
@@ -0,0 +1,36 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+suiteProperties.put('id', 'Test Suites/Auth Internal Data')
+suiteProperties.put('name', 'Auth Internal Data')
+suiteProperties.put('description', '')
+suiteProperties.put('rerunTestFailImmediately', 'false')
+suiteProperties.put('retryCount', '0')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("D:\\Coding\\Katalon\\project-awal\\Reports\\20230813_141411\\Auth Internal Data\\20230813_141411\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/Auth Internal Data', suiteProperties, new File("D:\\Coding\\Katalon\\project-awal\\Reports\\20230813_141411\\Auth Internal Data\\20230813_141411\\testCaseBinding"))
